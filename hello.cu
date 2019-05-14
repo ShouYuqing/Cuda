0c673e72f0fc612f40f50006838a99e88271c3f9
yq_mc <573380218@qq.com>
2019-05-14 15:53:05 -0400

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -22,7 +22,7 @@ int main()
 	// host data
 	int size = 10 * sizeof(float);
 	float *label = (float*)malloc(size);
-	label = [0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f];
+	label = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f};
 	float *err;
     err = (float*)malloc(size);
     label = (float*)malloc(size);
