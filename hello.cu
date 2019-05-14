6bd5188a05b01b7b43102aeabacee9908e020aae
yq_mc <573380218@qq.com>
2019-05-14 14:20:47 -0400

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -5,46 +5,48 @@
 #define BLOCK_SIZE THREADS_PER_BLOCK
 
 // calculation of loss
-__global__ void cal_loss(int *error, int *label, int N) {
+__global__ void cal_loss(int *err, int *label, int N) {
+
+	printf("threadIdx:(%d, %d, %d) blockIdx:(%d, %d, %d) blockDim:(%d, %d, %d) "
+	"gridDim:(%d, %d, %d)\n", threadIdx.x, threadIdx.y, threadIdx.z,
+	blockIdx.x, blockIdx.y, blockIdx.z, blockDim.x, blockDim.y, blockDim.z,
+	gridDim.x,gridDim.y,gridDim.z);
 	const int pos = blockIdx.x * blockDim.x + threadIdx.x;
 	const int totalPos = blockDim.x * gridDim.x;
 
-	for (int idx = N * pos / totalPos; idx < N * (pos+1) / totalPos; ++idx) { 
-		err[idx] = ((Y == idx ? 1.0f : 0.0f) - output[idx]); // calculation of error
-	}
+	//for (int idx = N * pos / totalPos; idx < N * (pos+1) / totalPos; ++idx) { 
+	//	err[idx] = ((Y == idx ? 1.0f : 0.0f) - output[idx]); // calculation of error
+	//}
 	return 0;
 }
 
 int main()
 {
+	// host data
 	int size = 10 * sizeof(float);
-	float *label = [0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f];
-    float *d_label;
-    cudaMalloc(&d_label, sizeof(float) * size);
-    cudaMemcpy(d_label, label, size, size, cudaMemcpyHostToDevice);
+	//float *label = [0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f];
+	float *label;
+	float *err;
+    err = (float*)malloc(size);
+    label = (float*)malloc(size);
 
-
-
-
-    cudaMalloc((void**)&d_in, size);
-    cudaMalloc((void**)&d_out, size);
-
-    in = (int*)malloc(size); random_ints(in, N);
-    out = (int*)malloc(size);
-    
-    cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice); // copy memory from CPU to GPU
-    cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
+    // copy data to device
+    float *d_label, *d_err;
+    cudaMalloc(&d_label, size);
+    cudaMalloc(&d_err, sizeof(float) * 10);
+    cudaMemcpy(d_label, label, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_err, err, size, cudaMemcpyHostToDevice);
+ 
+    cal_loss <<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_err, d_label, 10);// kernel function
   
-    stencil_1d <<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_in, d_out);// kernel function
-  
-    cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost); // copy the result from GPU to CPU
+    //cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost); // copy the result from GPU to CPU
 
-    for(int i=0; i<N; i++)
-        printf("%i ---i=%i \n", out[i], i);
+    //for(int i=0; i<N; i++)
+    //    printf("%i ---i=%i \n", out[i], i);
 
-    free(in); free(out);
+    free(label); free(err);
   
-    cudaFree(d_in); cudaFree(d_out);
+    cudaFree(d_label); cudaFree(d_err);
 
     return 0;
 }
